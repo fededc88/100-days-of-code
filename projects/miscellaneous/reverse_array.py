5b88c82be4ae3a0f318833f51e1dd2fbd5369706
Federico D. Ceccarelli <fededc88@gmail.com>
2023-12-05 22:13:39 -0500

    refactored reverse_array to handle list and str

--- projects/miscellaneous/reverse_array.py
+++ projects/miscellaneous/reverse_array.py
@@ -3,6 +3,15 @@
 #In other words, the function should not use an auxiliary array to do the work.
 
 
-def reverse_array_in_place(lst = list()):
-    lst.reverse()
+def reverse_array_in_place(lst):
+
+    # get lst class type
+    lst_type = type(lst)
+
+    if lst_type is list:
+        lst = lst[::-1]
+
+    elif lst_type is str:
+        lst = lst[::-1]
+    
     return lst   
--- projects/miscellaneous/test/test_reverse_array.py
+++ projects/miscellaneous/test/test_reverse_array.py
@@ -4,12 +4,13 @@ import reverse_array as ra
 
 class test_reverse_array_in_place(unittest.TestCase):
 
+    lst = ['1', '2', '3', 'p', 'r', 'o', 'b', 'a', 'n', 'd', 'o', '!']
+    lst_reverse = ['!','o', 'd','n', 'a', 'b', 'o', 'r', 'p', '3', '2', '1']
 
     def test_reverse_arrat_in_place(self):
-        lst = ['1', '2', '3', 'p', 'r', 'o', 'b', 'a', 'n', 'd', 'o', '!']
-        lst_reverse = ['!','o', 'd','n', 'a', 'b', 'o', 'r', 'p', '3', '2', '1']
 
-        self.assertListEqual(ra.reverse_array_in_place(lst), lst_reverse)
+        self.assertListEqual(ra.reverse_array_in_place(self.lst), self.lst_reverse)
+        self.assertMultiLineEqual(ra.reverse_array_in_place("1 2 3 probando!"), "!odnaborp 3 2 1")
 
 if __name__ == '__main__':
     unittest.main()

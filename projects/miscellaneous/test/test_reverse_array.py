b24e9e16c55f3d2ac885a8d17faceb8d8a0d91c8
Federico D. Ceccarelli <fededc88@gmail.com>
2023-12-07 21:13:52 -0500

    refactored the test

--- projects/miscellaneous/test/test_reverse_array.py
+++ projects/miscellaneous/test/test_reverse_array.py
@@ -18,16 +18,18 @@ class test_reverse_array_in_place(unittest.TestCase):
     # Write a function that finds the missing number in an unsorted array
     # containing every one of the other 99 numbers ranging from 1 to 100.
 
+        # create the list and shuffle it
         lst_missing = [d for d in range (1,100)]
         random.shuffle(lst_missing)
 
-        missing_idx = int(random.randrange(1,100))
+        # get a random missing object
+        missing_idx = int(random.randrange(0,99))
         
+        # save it for the testing and miss it
         missing_number = lst_missing[missing_idx]
-
         lst_missing.remove(missing_number)
 
-        self.assertListEqual(ra.find_missing_number(lst_missing, missing_number))
+        self.assertEqual(ra.find_missing_number(lst_missing), missing_number)
 
 if __name__ == '__main__':
     unittest.main()

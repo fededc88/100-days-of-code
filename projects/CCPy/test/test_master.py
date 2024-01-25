cef1a0fb7330eb2ffd60c85c31fb1830ca467b60
Federico D. Ceccarelli <fededc88@gmail.com>
2024-01-24 23:52:17 -0500

    added lost files:

---
+++ projects/CCPy/test/test_master.py
@@ -0,0 +1,9 @@
+import unittest
+import master as ccp
+
+class test_master(unittest.TestCase):
+    def setUp(self)
+        self.master = ccp.master()
+
+    def tearDown(self)
+        self.master.dispose()
